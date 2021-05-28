f86855958c8cea2c1294877c7d6e01678d6381f7
samroodAli <devsamrood@gmail.com>
2021-05-28 20:35:18 +0530

    Devise:Add user model with name

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  devise_for :users
+  get 'static_pages/home'
+  root 'static_pages#home'
 end
