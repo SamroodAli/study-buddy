6a1ebcacf2208089649f5c15be78d98409989977
samroodAli <devsamrood@gmail.com>
2021-05-29 18:19:16 +0530

    Seed:Add users to seed database

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,37 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+# Create a main sample user.
+User.create!( name:  "Samrood",
+              email: "example1@study.org",
+              password:              "passpass",
+              password_confirmation: "passpass",
+            )
+User.create!( name:  "Omar",
+              email: "example2@study.org",
+              password:              "passpass",
+              password_confirmation: "passpass",
+            )
+User.create!( name:  "Deepak",
+              email: "example3@study.org",
+              password:              "passpass",
+              password_confirmation: "passpass",
+            )
+            
+User.create!( name:  "Jean",
+              email: "example4@study.org",
+              password:              "passpass",
+              password_confirmation: "passpass",
+            )
+
+User.create!( name:  "Leon",
+              email: "example5@study.org",
+              password:              "passpass",
+              password_confirmation: "passpass",
+            )
+
+User.create!( name:  "Nasser",
+              email: "example6@study.org",
+              password:              "passpass",
+              password_confirmation: "passpass",
+            )
