4e886791f100ff067932d523d03e965fcdecaf05
samroodAli <devsamrood@gmail.com>
2021-05-29 10:27:41 +0530

    Bootstrap:Add bootstrap imports to app js

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,6 +7,9 @@ import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
+import 'bootstrap/dist/js/bootstrap'
+import "bootstrap/dist/css/bootstrap";
+import "../stylesheets/application"
 
 Rails.start()
 Turbolinks.start()
