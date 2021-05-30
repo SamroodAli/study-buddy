7f8b4f07935749341e532bf5c13b651dc56824b3
samroodAli <devsamrood@gmail.com>
2021-05-30 18:21:08 +0530

    Feat:Add index groups action in controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,10 @@
 Rails.application.routes.draw do
 
-  get 'groups/index'
-  get 'groups/show'
-  get 'groups/new'
   root 'users#show'
 
   devise_for :users
 
   resources :users, only: [:show]
   resources :study_sessions, only:[:index,:new,:create,:show]
+  resources :groups, only: [:index]
 end
