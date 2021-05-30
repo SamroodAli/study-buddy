8b5cde275ce4dbfae4b5c461dbf9293de8f82362
samroodAli <devsamrood@gmail.com>
2021-05-30 21:30:42 +0530

    Fix:Remove devise authentication in app controller and add before action and session helper

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,11 @@
 class ApplicationController < ActionController::Base
-  before_action :authenticate_user!
+  include SessionsHelper
+
+  def login_required
+    unless logged_in?
+      store_original_url
+      flash[:danger] = 'PLease log in'
+      redirect_to login_url
+    end
+  end
 end
