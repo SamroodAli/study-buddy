bc83276563897d603e92828ff0a0fecb1d732e26
samroodAli <devsamrood@gmail.com>
2021-05-30 21:50:20 +0530

    Feat:Create session helpers in class model

--- app/models/user.rb
+++ app/models/user.rb
@@ -33,4 +33,24 @@ class User < ApplicationRecord
     end
   end
 
+  def remember
+    self.remember_token = User.new_token
+    update_attribute(:remember_digest, User.digest(remember_token))
+    remember_digest
+  end
+
+  def authenticated?(remember_token)
+    return false unless remember_digest
+
+    BCrypt::Password.new(remember_digest).is_password?(remember_token)
+  end
+
+  def session_token
+    remember_digest || remember
+  end
+
+  def forget
+    update_attribute(:remember_digest, nil)
+  end
+
 end
