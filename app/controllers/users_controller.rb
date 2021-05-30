7534d9c46cda8e8e9257cf4b06c8772ceff9e0e3
samroodAli <devsamrood@gmail.com>
2021-05-30 21:21:29 +0530

    User:Create user update action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -15,12 +15,23 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
     if @user.valid? && @user.save
       log_in @user
-      flash[:success] = 'Welcome to the Study Buddy'
+      flash[:success] = 'Welcome to Study Buddy'
       redirect_to @user
     else
       render :new
     end
   end
+
+  def update
+    if @user.update(user_params)
+      flash[:success] = 'Profile updated'
+      redirect_to @user
+    else
+      flash[:danger] = 'Please provide valid entries'
+      render :edit
+    end
+  end
+
 
   def destroy
     User.find_by(id: params[:id]).destroy
