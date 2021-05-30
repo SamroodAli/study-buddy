6f8c9250194f333cae636a4eb9f99423dec8e278
samroodAli <devsamrood@gmail.com>
2021-05-30 21:40:53 +0530

    Helper:Add user helper for sign up button

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -4,4 +4,12 @@ module UsersHelper
     gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}?s=#{size}"
     image_tag(gravatar_url, alt: user.name, class: 'gravatar')
   end
+
+  def user_helper(user)
+    if @user.new_record?
+      "Create my account"
+    else
+      "Update my account"
+    end
+  end
 end
