9df02528e3dcfc7e25f3a4e27c7ad380bbbf8de5
samroodAli <devsamrood@gmail.com>
2021-05-30 16:38:45 +0530

    Fix:Remove minitest test files

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,7 @@
+class SessionsController < ApplicationController
+  def index
+  end
+
+  def show
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
 
+  get 'sessions/index'
+  get 'sessions/show'
+  get 'users/show'
   devise_for :users
   resources :users, only: [:show]
+  root 'users#show'
 end
