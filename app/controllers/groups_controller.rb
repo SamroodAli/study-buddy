d66ad29f3d28f9f85e2b9939aba53ef2c8caa5f8
samroodAli <devsamrood@gmail.com>
2021-05-30 18:23:56 +0530

    Feat:Add create controller and strong parameters for group

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -9,4 +9,15 @@ class GroupsController < ApplicationController
   def new
     @group = current_user.groups.build
   end
+
+
+  def create
+    current_user.groups.create(group_params)
+  end
+
+
+  private
+  def group_params
+    params.require(:group).permit(:name)
+  end
 end
