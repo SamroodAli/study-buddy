52986272b7b5020c254ce08a14f23b97b9d2f418
samroodAli <devsamrood@gmail.com>
2021-05-30 20:56:44 +0530

    User:Remove devise from the project for custom authentication

--- app/controllers/users_controller.rb
+++
@@ -1,6 +0,0 @@
-class UsersController < ApplicationController
-  def show
-    @user = current_user
-  end
-
-end
--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,6 @@ Rails.application.routes.draw do
 
   root 'users#show'
 
-  devise_for :users
-
-  resources :users, only: [:show]
   resources :study_sessions, only:[:index,:new,:create,:show]
   resources :groups, only: [:index,:new,:create]
 end
