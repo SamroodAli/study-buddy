d153cff3e8f271fe46e3ff4ec4fe5a8a4db0e55e
samroodAli <devsamrood@gmail.com>
2021-05-30 16:53:09 +0530

    Session:Add new session form

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,6 +3,11 @@ class SessionsController < ApplicationController
   def index
     @sessions = current_user.sessions
   end
+  
+  def new
+    @session = current_user.sessions.build
+  end
+
 
   def show
   end
--- config/routes.rb
+++ config/routes.rb
@@ -5,3 +5,4 @@ Rails.application.routes.draw do
   resources :users, only: [:show]
   resources :sessions, only: [:index,:new]
 end
+  
