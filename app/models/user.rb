72b4ecd029644b175b0c447fc81bb9db5445eddc
samroodAli <devsamrood@gmail.com>
2021-05-30 21:48:15 +0530

    Fix:Restablish relations between other models for user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,8 @@
 class User < ApplicationRecord
+  has_many :study_sessions
+  has_many :groups
+  has_many :collaborations, foreign_key: :participant_id
+  has_many :collaborative_sessions, through: :collaborations, source: :study_session
+  has_many :memberships, foreign_key: :member_id
+  has_many :participating_groups, through: :memberships
 end
