dadcc8afbd1729189272a2c7c43d04150bfa0e5f
samroodAli <devsamrood@gmail.com>
2021-05-30 21:29:03 +0530

    User:Use session helpers from my template app for authentication

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,2 +1,49 @@
 module SessionsHelper
+  def log_in(user)
+    session[:user_id] = user.id
+    session[:session_token] = user.session_token
+  end
+
+  def current_user
+    if (user_id = session[:user_id])
+      user = User.find_by(id: session[:user_id])
+      @current_user ||= user if user&.session_token == session[:session_token]
+    elsif (user_id = cookies.encrypted[:user_id])
+      user = User.find_by(id: user_id)
+      if user&.authenticated?(cookies[:remember_token])
+        log_in user
+        @current_user = user
+      end
+    end
+  end
+
+  def logged_in?
+    !current_user.nil?
+  end
+
+  def remember(user)
+    user.remember
+    cookies.permanent.encrypted[:user_id] = user.id
+    cookies.permanent[:remember_token] = user.remember_token
+  end
+
+  def forget(user)
+    user.forget
+    cookies.delete(:user_id)
+    cookies.delete(:remember_token)
+  end
+
+  def log_out
+    forget(current_user)
+    reset_session
+    @current_user = nil
+  end
+
+  def correct_user?(user)
+    user && user == current_user
+  end
+
+  def store_original_url
+    session[:forwarding_url] = request.original_url if request.get?
+  end
 end
