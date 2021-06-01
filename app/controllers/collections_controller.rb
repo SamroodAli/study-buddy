40238b2b2e9b6ea00970c4dcf7f9ff9c80484a32
samroodAli <devsamrood@gmail.com>
2021-06-01 15:12:09 +0530

    Config:Create stylelintrc file and fix rubocop errors

--- app/controllers/collections_controller.rb
+++ app/controllers/collections_controller.rb
@@ -19,7 +19,7 @@ class CollectionsController < ApplicationController
   def new_session
     current_collection = Collection.find(params[:id])
     @collections = current_user.collections.map { |collection| [collection.name, collection.id] }
-    current_collection_id = @collections.find_index([current_collection.name,current_collection.id])
+    current_collection_id = @collections.find_index([current_collection.name, current_collection.id])
     @selected = @collections[current_collection_id]
     @study_session = current_user.study_sessions.build
     render 'study_sessions/new'
