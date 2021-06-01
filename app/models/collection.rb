a0e16890ae182fcf6c6a474358b35b0fca88d5c5
samroodAli <devsamrood@gmail.com>
2021-06-01 15:15:54 +0530

    Icon:Create association between collection and icon using has_one_attached

--- app/models/collection.rb
+++ app/models/collection.rb
@@ -1,8 +1,10 @@
 class Collection < ApplicationRecord
   scope :internal, -> { where(external: false) }
+  scope :external, -> { find_by(external: true) }
   has_many :study_sessions
   belongs_to :user
   has_many :memberships, foreign_key: :participating_collection_id
   has_many :members, through: :memberships
-  scope :external, -> { find_by(external: true) }
+
+  has_one_attached :image
 end
