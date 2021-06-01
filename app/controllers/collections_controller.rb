7c4a10ca39d0af44946df8c12c21509a6a505120
samroodAli <devsamrood@gmail.com>
2021-06-01 14:55:38 +0530

    Session:Populate existing collection in form select for new sessions

--- app/controllers/collections_controller.rb
+++ app/controllers/collections_controller.rb
@@ -16,11 +16,13 @@ class CollectionsController < ApplicationController
     @collection = current_user.collections.build
   end
 
-  def new_group_session
+  def new_session
+    current_collection = Collection.find(params[:id])
     @collections = current_user.collections.map { |collection| [collection.name, collection.id] }
-    @selected = @collections[Collection.find(params[:id])]
+    current_collection_id = @collections.find_index([current_collection.name,current_collection.id])
+    @selected = @collections[current_collection_id]
     @study_session = current_user.study_sessions.build
-    render 'study_session#new'
+    render 'study_sessions/new'
   end
 
   def create
--- config/routes.rb
+++ config/routes.rb
@@ -13,7 +13,7 @@ Rails.application.routes.draw do
   end
   resources :collections, only: [:index,:new,:create,:show] do
     get :all , on: :collection
-    get :new_group_session, on: :member
+    get :new_session, on: :member
   end
   resources :users
 end
