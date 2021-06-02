12b2b2eb149be97367ad76161c32cec18d240b0d
samroodAli <devsamrood@gmail.com>
2021-06-02 18:40:50 +0530

    Style:Style session show page

--- app/helpers/study_sessions_helper.rb
+++ app/helpers/study_sessions_helper.rb
@@ -1,2 +1,11 @@
 module StudySessionsHelper
+
+  def image(session)
+    if session.collection.image.attached?
+      image_tag session.collection.image, class:"icon user-icon"
+    else
+      nil
+    end
+  end
 end
+
