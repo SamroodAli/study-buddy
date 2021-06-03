8ce66f51e96fea55f9d845644b7f3214b7212c61
samroodAli <devsamrood@gmail.com>
2021-06-03 19:16:06 +0530

    Fix:Sort option in show page of sessions fix

--- app/helpers/study_sessions_helper.rb
+++ app/helpers/study_sessions_helper.rb
@@ -6,4 +6,13 @@ module StudySessionsHelper
   def duration(study_session)
     "Total duration : #{distance_of_time_in_words(study_session.duration)}"
   end
+
+  def collections_page?
+    request.url.include?("collections")
+  end
+
+  def sessions_page?
+    url = request.url
+    url.include?("sessions") && !url.include?("no_sort")
+  end
 end
