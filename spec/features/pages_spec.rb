a7bf913b0ae583930ece381895c2ad9e3bd0c8e9
samroodAli <devsamrood@gmail.com>
2021-06-03 18:22:57 +0530

    Fix:Format code using rubocop linter

--- spec/features/pages_spec.rb
+++ spec/features/pages_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 require './spec/support/helpers'
 
-feature 'Study Buddy integration testing' do
+feature 'Study Buddy home integration testing' do
   fixtures :users
   background do
     sam = User.new(name: 'samrood', email: 'user@example.com', password: 'password')
@@ -10,5 +10,9 @@ feature 'Study Buddy integration testing' do
 
   scenario 'home page has blue background' do
     expect(page).to have_selector('.nav-blue')
+  end
+
+  scenario 'home page has navigational links' do
+    (page).to_not have_link("All My study sessions")
   end
 end
