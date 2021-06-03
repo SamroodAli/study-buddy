b57f947b86136dd663fc00909289685fc01d5ac8
samroodAli <devsamrood@gmail.com>
2021-06-03 17:22:39 +0530

    Test:User model with validity checks using seeds

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,8 +1,38 @@
 require 'rails_helper'
 
 RSpec.describe User do
-
-  subject(:user) { users(:John) }
+  fixtures :users
+  subject(:user) { users(:Samrood) }
   let(:invalid_user) { User.new }
+
+  describe 'Validity with name,password and email from fixtures' do
+    it 'should be valid from fixtures' do
+      expect(user).to be_valid
+    end
+  end
+
+  describe :invalid_name do
+    it 'should have valid name' do
+      user = User.new(name: '', email: 'some@gmail.com')
+      user.valid?
+      expect(user.errors.full_messages).to include("Name can't be blank")
+      expect(user).not_to be_valid
+    end
+  end
+
+  describe :invalid_password do
+    it 'should have valid name' do
+      user = User.new(name: 'samrood', email: 'some@gmail.com')
+      user.valid?
+      expect(user.errors.full_messages).to include("Password can't be blank")
+      expect(user).not_to be_valid
+    end
+  end
+
+  describe :invalidity do
+    it 'should not be valid with blank fields' do
+      expect(invalid_user).not_to be_valid
+    end
+  end
 
 end
