e5d24ad97b7ee8a678f0ca18b430a01a97073f65
samroodAli <devsamrood@gmail.com>
2021-06-03 17:47:10 +0530

    Test:Test validity of fields in study sessions

--- spec/models/study_sessions_spec.rb
+++ spec/models/study_sessions_spec.rb
@@ -2,6 +2,7 @@ require 'rails_helper'
 
 RSpec.describe Collection do
   fixtures :study_sessions
+  fixtures :users
   subject(:Algorithm) { study_sessions(:Algorithm) }
   let(:invalid_session) { StudySession.new }
 
@@ -21,10 +22,46 @@ RSpec.describe Collection do
   end
 
   describe :invalid_name do
-    it 'should have valid name' do
+    it 'should have valid author' do
       study_session = StudySession.new(name: 'Algorithm')
       study_session.valid?
       expect(study_session.errors.full_messages).to include("Author must exist")
+      expect(study_session).not_to be_valid
+    end
+  end
+
+  describe :invalid_collection do
+    it 'should be in a valid collection' do
+      study_session = StudySession.new(name: 'Algorithm',author:users(:Samrood))
+      study_session.valid?
+      expect(study_session.errors.full_messages).to include("Collection must exist")
+      expect(study_session).not_to be_valid
+    end
+  end
+
+  describe :invalid_duration_start do
+    it 'should be valid' do
+      study_session = StudySession.new(name: 'Algorithm',author:users(:Samrood))
+      study_session.valid?
+      expect(study_session.errors.full_messages).to include("Duration start can't be blank")
+      expect(study_session).not_to be_valid
+    end
+  end
+
+  describe :invalid_duration_end do
+    it 'should be valid' do
+      study_session = StudySession.new(name: 'Algorithm',author:users(:Samrood))
+      study_session.valid?
+      expect(study_session.errors.full_messages).to include("Duration end can't be blank")
+      expect(study_session).not_to be_valid
+    end
+  end
+
+  describe :invalid_duration do
+    it 'should be valid' do
+      study_session = StudySession.new(name: 'Algorithm',author:users(:Samrood))
+      study_session.valid?
+      expect(study_session.errors.full_messages).to include("Duration can't be blank")
       expect(study_session).not_to be_valid
     end
   end
