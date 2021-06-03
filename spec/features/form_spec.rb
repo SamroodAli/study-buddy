f2476e24ba9a601da9e97d3678d4a94416c8ec4d
samroodAli <devsamrood@gmail.com>
2021-06-03 19:33:46 +0530

    Style:Style show page study session

--- spec/features/form_spec.rb
+++ spec/features/form_spec.rb
@@ -1,5 +1,17 @@
 require 'rails_helper'
+require './spec/support/helpers'
 
-RSpec.feature "Forms", type: :feature do
-  pending "add some scenarios (or delete) #{__FILE__}"
+feature 'Study Buddy new session and new collection' do
+  fixtures :users
+  fixtures :collections
+  fixtures :study_sessions
+  background do
+    sam = User.new(name: 'samrood', email: 'user@example.com', password: 'password')
+    sign_up_with(sam)
+  end
+  
+  scenario 'Creating new session' do
+    visit new_study_session_path
+    expect(page).to have_content("enter name")
+  end
 end
