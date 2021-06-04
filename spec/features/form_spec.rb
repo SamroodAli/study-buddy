299aa67c66ae435ad6f17c50cbe3990513b522ce
samroodAli <devsamrood@gmail.com>
2021-06-04 14:47:23 +0530

    Feat:Negative test for new collection creation

--- spec/features/form_spec.rb
+++ spec/features/form_spec.rb
@@ -40,21 +40,19 @@ feature 'Study Buddy new session and new collection' do
     expect(StudySession.count).not_to eq(count_before + 1)
   end
 
-  scenario 'Creating an session with no duration end' do
-    count_before = StudySession.count
-    visit new_study_session_path
-    fill_in 'Enter session name', with: 'Learning rails'
-    fill_in 'Duration start', with: '03:00'
-    fill_in 'Duration end', with: ''
-    click_button 'Create new session'
-    expect(StudySession.count).not_to eq(count_before + 1)
-  end
-
   scenario 'Creating new collection' do
     count_before = Collection.count
     visit new_collection_path
     fill_in 'Enter collection name', with: 'learning algorithms'
     click_button 'Create new collection'
     expect(Collection.count).to eq(count_before + 1)
+  end
+
+  scenario 'Creating new collection' do
+    count_before = Collection.count
+    visit new_collection_path
+    fill_in 'Enter collection name', with: ''
+    click_button 'Create new collection'
+    expect(Collection.count).not_to eq(count_before + 1)
   end
 end
