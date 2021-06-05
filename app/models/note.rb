742436e28d94a77ad9a8e844c6af48fd5b738ee5
samroodAli <devsamrood@gmail.com>
2021-06-05 14:59:20 +0530

    Note:Connection of note with user and collection

--- app/models/note.rb
+++ app/models/note.rb
@@ -1,2 +1,4 @@
 class Note < ApplicationRecord
+  belongs_to :user
+  belongs_to :collection, optional: true
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_06_01_125558) do
+ActiveRecord::Schema.define(version: 2021_06_05_092736) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -61,6 +61,15 @@ ActiveRecord::Schema.define(version: 2021_06_01_125558) do
   create_table "memberships", force: :cascade do |t|
     t.integer "participating_collection_id"
     t.integer "member_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "notes", force: :cascade do |t|
+    t.text "content"
+    t.string "title"
+    t.integer "user_id"
+    t.integer "collection_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
