52a5bce4e0a823e6593f4f49a3fe15e4d866d145
samroodAli <devsamrood@gmail.com>
2021-06-05 15:28:54 +0530

    Note:Index action for notes

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -5,9 +5,26 @@ class NotesController < ApplicationController
   end
 
   def index
+    @notes = current_user.notes.all
   end
 
   def new
     current_user.notes.build
+  end
+
+  def create
+    @note = current.notes.create!(notes_params)
+    if @note.valid? && @note.save
+      flash[:success] =  "Note #{@note.title} saved successfully"
+      redirect_to @note
+    else
+      flash[:danger]  = "Invalid notes entry"
+      render :new
+    end
+  end
+
+  private
+  def notes_params
+    params.require(:notes).permit(:title,:content)
   end
 end
