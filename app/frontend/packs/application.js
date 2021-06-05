97d7a9fc649800d89a0c469feb560c9458cf35d3
samroodAli <devsamrood@gmail.com>
2021-06-05 20:28:57 +0530

    Bootstrap:Import bootstrap js to app js

--- app/frontend/packs/application.js
+++ app/frontend/packs/application.js
@@ -7,8 +7,9 @@ import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
-import 'bootstrap/dist/js/bootstrap'
-import "bootstrap/dist/css/bootstrap";
+// import 'bootstrap/dist/js/bootstrap'
+// import "bootstrap/dist/css/bootstrap";
+import '../js/bootstrap_js_files.js'  
 Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
