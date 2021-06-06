a8e50b1b167ad4c2ab16f0a2915928e3e8eac112
samroodAli <devsamrood@gmail.com>
2021-06-06 18:15:18 +0530

    Fix:Fix precompile issue in heroku

--- config/application.rb
+++ config/application.rb
@@ -18,5 +18,7 @@ module StudyBuddy
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+    config.assets.initialize_on_precompile = false
+
   end
 end
